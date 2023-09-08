381974615987dc5a15aa15a98faf37697d8fbf8b
Staneo644 <aurele.josserand@gmail.com>
2023-09-08 14:29:23 +0200

    delete des trucs

--- project/back/app/app/dist/users/users.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'mongoose';
-import { User, UserDocument } from './users.model';
-export declare class UsersService {
-    private readonly userModel;
-    constructor(userModel: Model<UserDocument>);
-    createUser(username: string, password: string): Promise<User>;
-    getUser(query: object): Promise<User>;
-}
